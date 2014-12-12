62fed71a3aefb38a6326147d8efa1adc81b414ff
Julien Cavanna <julient.cavanna@y-nov.com>
2014-12-12 19:01:08 +0100

    Ajout du Projet

---
+++ Altis-Life-master/arma3life-3.1.4.6.sql
@@ -0,0 +1,2 @@
+hgALTER TABLE `players` ADD `med_gear` TEXT NOT NULL AFTER `cop_gear`;
+UPDATE `players` SET `med_gear`='"[]"';
---
+++ Altis-Life-master/arma3life-3.1.4.8.sql
@@ -0,0 +1,8 @@
+fsdDELIMITER $$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `deleteOldGangs`()
+BEGIN
+	DELETE FROM `gangs` WHERE `active` = 0;
+END$$
+
+DELIMITER ;
